c22e34563b3ba5fbc5b2674ce6ceb4b95cc626d0
Linting Xue <lintingx@google.com>
2022-02-24 10:58:50 -0800

    Internal changes.
    PiperOrigin-RevId: 430744241

--- byt5/get_length_stats.py
+++ byt5/get_length_stats.py
@@ -1,4 +1,4 @@
-# Copyright 2021 The ByT5 Authors.
+# Copyright 2022 The ByT5 Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ to 1.1 * 99th percentile length.
 
 from absl import app
 from absl import flags
+import byt5.sigmorphon  # pylint:disable=unused-import
 import byt5.tasks  # pylint:disable=unused-import
 import multilingual_t5.tasks  # pylint:disable=unused-import
 import t5
